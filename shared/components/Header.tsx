80eeaafa09f224ec6bc077ed95a75b906185cd54
akmatoff <akmatoff.exe@gmail.com>
2024-12-13 01:25:33 +0600

    fix header

--- shared/components/Header.tsx
+++ shared/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-center w-screen">
+    <header className="flex justify-center w-full">
       <div className="flex items-center justify-between mid-content py-5">
         <div className="font-semibold text-xl md:text-2xl">
           <Link href="/">CarDealerApp</Link>
