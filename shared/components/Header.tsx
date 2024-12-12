d4c1d9d8b44dd34c55bd959f6098d7c5ab546ebd
akmatoff <akmatoff.exe@gmail.com>
2024-12-13 00:46:26 +0600

    Add link to home page

--- shared/components/Header.tsx
+++ shared/components/Header.tsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex justify-center w-screen">
-      <div className="mid-content py-5">
+      <div className="flex items-center justify-between mid-content py-5">
         <div className="font-semibold text-xl md:text-2xl">
           <Link href="/">CarDealerApp</Link>
         </div>
+
+        <Link href="/">Home</Link>
       </div>
     </header>
   );
